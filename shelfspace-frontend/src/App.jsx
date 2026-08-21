b96e72dadfda35e13a39a49154ef35913c16ca2b
juswadenyel <bodeganinoreen@gmail.com>
2026-08-21 14:59:35 +0800

    Add dashboard page

--- shelfspace-frontend/src/App.jsx
+++ shelfspace-frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
+import Dashboard from './pages/Dashboard'
 import Login from './pages/Login'
 import Register from './pages/Register'
 
@@ -7,6 +8,7 @@ function App() {
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/dashboard" element={<Dashboard />} />
       <Route path="*" element={<Navigate to="/login" replace />} />
     </Routes>
   )
